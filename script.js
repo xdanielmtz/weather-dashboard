c67203437a8d547ba2bf2faf0dee601151faf0e0
Dani JM <xdanielmtz@gmail.com>
2020-10-12 01:59:06 -0400

    added the uv index by adding a sperate API and combining it with the original API

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function cityName(){
     cityH1.text(theCity);
     var APIKey = "166a433c57516f51dfab1f7edaed8413";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + theCity + "&units=imperial&appid=" + APIKey;
+   
 
     $.ajax({
       url: queryURL,
@@ -22,16 +23,25 @@ function cityName(){
      tempEL.text("Temperature: "+ (response.main.temp) + " ºF");        
      humEL.text("Humidity: "+ (response.main.humidity) + " %");
      windEL.text("Wind Speed: " + (response.wind.speed) + " MPH");
-    //  uvEl.text("UV Index: " + (response.))
      var lat = (response.coord.lat);
-     console.log(lat);
-   })
+     var lon = (response.coord.lon);
 
 
+     var queryURLTwo = "http://api.openweathermap.org/data/2.5/uvi?" + "lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+     $.ajax({
+         url: queryURLTwo,
+         method: "GET"
+     }).then(function(response){
+         uvEl.text("UV Index: " + (response.value));
+
+     })
+
+    })
+
+    
 
 
-    });
-
+   });
   
   }
   
